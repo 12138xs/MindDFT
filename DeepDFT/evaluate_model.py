707818f295d799a976107813400a314074268ad5
DavidDai111 <1776607423@qq.com>
2024-06-04 19:22:42 +0800

    Loading dataset and evaluate model
    1. 导入了ms版本的dataset.py
    2. 完成了evaluate_model部分迁移工作，concatdataset方法待实现
    3. 当batch内没有需要被预测或推断的边缘时，传入网络的张量维度中包含0导致部分算子报错

--- DeepDFT/evaluate_model.py
+++ DeepDFT/evaluate_model.py
@@ -4,7 +4,8 @@ import tarfile
 import logging
 
 import numpy as np
-import torch
+import mindspore as ms
+from mindspore import ops
 
 import dataset
 import densitymodel
@@ -26,10 +27,16 @@ def get_arguments(arg_list=None):
     parser.add_argument("--probe_count", type=int, default=1000)
     parser.add_argument("--write_error_cubes", action="store_true")
     parser.add_argument(
-        "--device",
+        "--device_target",
         type=str,
-        default="cuda",
-        help="Set which device to use for training e.g. 'cuda' or 'cpu'",
+        default="GPU",
+        help="Specify the type of device to be used for inference e.g. 'Ascend', 'GPU', or 'CPU'",
+    )
+    parser.add_argument(
+        "--device_id",
+        type=int,
+        default=0,
+        help="Specify the device number to be used for inference e.g. '0' or '1'",
     )
     parser.add_argument(
         "--ignore_pbc",
@@ -44,6 +51,14 @@ def get_arguments(arg_list=None):
 
     return parser.parse_args(arg_list)
 
+def batch_map_fn(data):
+    batch_data = {}
+    for key, value in data[0].items():
+        if isinstance(value, ms.Tensor):
+            batch_data[key] = value.asnumpy()
+        else:
+            batch_data[key] = value
+    return batch_data
 
 def main():
     args = get_arguments()
@@ -63,16 +78,19 @@ def main():
         handlers=handlers,
     )
 
+    device_target = args.device_target
+    device_id = args.device_id
+    ms.set_context(device_target=device_target, device_id=device_id)
+
     # Initialise model and load model
-    device = torch.device(args.device)
     if args.use_painn_model:
         net = densitymodel.PainnDensityModel(args.num_interactions, args.node_size, args.cutoff,)
     else:
         net = densitymodel.DensityModel(args.num_interactions, args.node_size, args.cutoff,)
-    net = net.to(device)
     logging.info("loading model from %s", args.load_model)
-    state_dict = torch.load(args.load_model)
-    net.load_state_dict(state_dict["model"])
+
+    state_dict = ms.load_checkpoint(args.load_model)
+    param_not_load, _ = ms.load_param_into_net(net, state_dict)
 
     # Load dataset
     if args.dataset.endswith(".txt"):
@@ -82,7 +100,9 @@ def main():
     else:
         filelist = [args.dataset]
     logging.info("loading data %s", args.dataset)
-    densitydata = torch.utils.data.ConcatDataset([dataset.DensityData(path) for path in filelist])
+    # densitydata = ms.dataset.ConcatDataset([ms.dataset.GeneratorDataset(dataset.DensityData(path), column_names=["densitydata"]) for path in filelist])
+    densitydata = ms.dataset.GeneratorDataset(dataset.DensityData(filelist[0]), column_names=["densitydata"], shuffle=False)
+    # densitydata = dataset.DensityData(filelist[0])
 
     # Split data into train and validation sets
     if args.split_file:
@@ -90,16 +110,11 @@ def main():
     else:
         datasplits = {"all": densitydata}
 
-
     for split_name, densitydataset in datasplits.items():
         if args.split and split_name not in args.split:
             continue
-        dataloader = torch.utils.data.DataLoader(
-            densitydataset,
-            1,
-            num_workers=4,
-            collate_fn=lambda x: x[0],
-        )
+
+        dataloader = densitydataset.batch(batch_size=1, num_parallel_workers=4)
 
         if args.write_error_cubes:
             outname = os.path.join(args.output_dir, "eval_" + split_name + ".tar")
@@ -107,62 +122,60 @@ def main():
 
         for density_dict in dataloader:
             density = []
-            with torch.no_grad():
-                # Loop over all slices
-                density_iter = dataset.DensityGridIterator(density_dict, args.ignore_pbc, args.probe_count, args.cutoff)
+            density_dict = batch_map_fn(density_dict)
 
-                # Make graph with no probes
-                collate_fn = dataset.CollateFuncAtoms(
-                    cutoff=args.cutoff,
-                    pin_memory=True,
-                    disable_pbc=args.ignore_pbc,
-                )
-                graph_dict = collate_fn([density_dict])
-                device_batch = {
-                    k: v.to(device=device, non_blocking=True) for k, v in graph_dict.items()
-                }
+            # Loop over all slices
+            density_iter = dataset.DensityGridIterator(density_dict, args.probe_count, args.cutoff, args.ignore_pbc)
+
+            # Make graph with no probes
+            collate_fn = dataset.CollateFuncAtoms(
+                cutoff=args.cutoff,
+                set_pbc_to=args.ignore_pbc,
+            )
+            device_batch = collate_fn([density_dict])
+
+            if args.use_painn_model:
+                atom_representation_scalar, atom_representation_vector = net.atom_model(device_batch)
+            else:
+                atom_representation = net.atom_model(device_batch)
+
+            num_positions = np.prod(density_dict["grid_position"].shape[0:3])
+            sum_abs_error = ms.Tensor(0, dtype=ms.float32)
+            sum_squared_error = ms.Tensor(0, dtype=ms.float32)
+            sum_target = ms.Tensor(0, dtype=ms.float32)
+
+            for slice_id, probe_graph_dict in enumerate(density_iter):
+                # Transfer target to device
+                flat_index = np.arange(slice_id*args.probe_count, min((slice_id+1)*args.probe_count, num_positions))
+                pos_index = np.unravel_index(flat_index, density_dict["density"].shape[0:3])
+                probe_target = ms.Tensor(density_dict["density"][pos_index], dtype=ms.float32)
+
+                # Transfer model input to device
+                probe_dict = dataset.collate_list_of_dicts([probe_graph_dict])
+                device_batch["probe_edges"] = probe_dict["probe_edges"]
+                device_batch["probe_edges_displacement"] = probe_dict["probe_edges_displacement"]
+                device_batch["probe_xyz"] = probe_dict["probe_xyz"]
+                device_batch["num_probe_edges"] = probe_dict["num_probe_edges"]
+                device_batch["num_probes"] = probe_dict["num_probes"]
+
                 if args.use_painn_model:
-                    atom_representation_scalar, atom_representation_vector = net.atom_model(device_batch)
+                    # res = net.probe_model(device_batch["probe_xyz"], atom_representation_scalar, atom_representation_vector, **device_batch)
+                    res = net.probe_model.construct_and_gradients(device_batch, atom_representation_scalar, atom_representation_vector)
                 else:
-                    atom_representation = net.atom_model(device_batch)
+                    # res = net.probe_model(device_batch["probe_xyz"], atom_representation, **device_batch)
+                    res = net.probe_model.construct_and_gradients(device_batch, atom_representation)
 
-                num_positions = np.prod(density_dict["grid_position"].shape[0:3])
-                sum_abs_error = torch.tensor(0, dtype=torch.double, device=device)
-                sum_squared_error = torch.tensor(0, dtype=torch.double, device=device)
-                sum_target = torch.tensor(0, dtype=torch.double, device=device)
-                for slice_id, probe_graph_dict in enumerate(density_iter):
-                    # Transfer target to device
-                    flat_index = np.arange(slice_id*args.probe_count, min((slice_id+1)*args.probe_count, num_positions))
-                    pos_index = np.unravel_index(flat_index, density_dict["density"].shape[0:3])
-                    probe_target = torch.tensor(density_dict["density"][pos_index]).to(device=device, non_blocking=True)
+                # Compare result with target
+                error = probe_target - res
+                sum_abs_error += ops.sum(ops.abs(error))
+                sum_squared_error += ops.sum(ops.square(error))
+                sum_target += ops.sum(probe_target)
 
-                    # Transfer model input to device
-                    probe_dict = dataset.collate_list_of_dicts([probe_graph_dict])
-                    probe_dict = {
-                        k: v.to(device=device, non_blocking=True) for k, v in probe_dict.items()
-                    }
-                    device_batch["probe_edges"] = probe_dict["probe_edges"]
-                    device_batch["probe_edges_displacement"] = probe_dict["probe_edges_displacement"]
-                    device_batch["probe_xyz"] = probe_dict["probe_xyz"]
-                    device_batch["num_probe_edges"] = probe_dict["num_probe_edges"]
-                    device_batch["num_probes"] = probe_dict["num_probes"]
-
-                    if args.use_painn_model:
-                        res = net.probe_model(device_batch, atom_representation_scalar, atom_representation_vector)
-                    else:
-                        res = net.probe_model(device_batch, atom_representation)
-
-                    # Compare result with target
-                    error = probe_target - res
-                    sum_abs_error += torch.sum(torch.abs(error))
-                    sum_squared_error += torch.sum(torch.square(error))
-                    sum_target += torch.sum(probe_target)
-
-                    if args.write_error_cubes:
-                        density.append(res.detach().cpu().numpy())
+                if args.write_error_cubes:
+                    density.append(res.detach().cpu().numpy())
 
             voxel_volume = density_dict["atoms"].get_volume()/np.prod(density_dict["density"].shape)
-            rmse = torch.sqrt((sum_squared_error/num_positions))
+            rmse = ops.sqrt((sum_squared_error/num_positions))
             mae = sum_abs_error/num_positions
             abserror_integral = sum_abs_error*voxel_volume
             total_integral = sum_target*voxel_volume
